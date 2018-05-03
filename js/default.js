b8688251a72aeeb24cc9b9da8ee00bd360637b1d
Matthew <matthew@matthew-dev>
2018-05-03 22:13:00 +1000

    Updated portfolio with latest Base

--- js/default.js
+++ js/default.js
@@ -1,1 +1,1 @@
-"querySelector"in document&&"addEventListener"in window&&(document.documentElement.className+="js"),WebFont.load({google:{families:["Lato:400,700"]}});
+"use strict";"querySelector"in document&&"addEventListener"in window&&(document.documentElement.className+="js"),WebFont.load({google:{families:["Lato:400,700"]}});
