74d65c55140ceaa9177b2c01ff2262fc08115175
Matthew Hartman <matthewhartmanau@gmail.com>
2019-09-23 07:17:24 +1000

    Minor update to portfolio

--- src/js/default.js
+++ src/js/default.js
@@ -1,13 +1,25 @@
 // Default JavaScript Functions and Initiations
 import WebFont from 'webfontloader'
 
-if ( 'querySelector' in document && 'addEventListener' in window ) {
-  document.documentElement.className += 'js';
-}
-
 // Load Custom Google Font
 WebFont.load({
   google: {
     families: ['Lato:400,700']
   }
 });
+
+(function() {
+  'use strict'
+  var header = document.querySelector('.header');
+  var colours = ['orangered', 'black', 'midnightblue', 'darkslateblue', 'indigo', 'crimson']
+  var random = Math.random();
+  var totalColours = colours.length;
+  var randomIndex = Math.floor(random * totalColours);
+  var randomColour = colours[randomIndex];
+  header.style.background = randomColour;
+  var style = document.querySelector('head').insertAdjacentHTML('beforeend', '<style type="text/css"> \
+      a { color: ' + randomColour + '; } \
+      a:hover, a:focus { color: ' + randomColour + '; } \
+    </style> \
+  ');
+}())
